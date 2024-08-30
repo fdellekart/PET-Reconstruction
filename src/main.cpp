24caa8d0ca9f4c6f8c50ea1debf7ab98ba753095
fdellekart <florian.dellekart@hotmail.de>
2024-08-30 10:21:07 +0200

    feat(input): Read integers from file one after the other

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 int main(int argc, char **argv) {
     std::ifstream input_stream;
 
-    input_stream.open("test.txt", std::ifstream::in);
+    auto filename = "/home/recon/Documents/Thesis-Dellekart/SIRF-STIR-PET-Reconstruction/input/1.3.12.2.1107.5.2.38.51035.30000017121607191856600000009.bf";
 
-    char c = input_stream.get();
+    input_stream.open(filename, std::ifstream::in);
 
+    int tag = 0;
+    int value;
+    
     while (input_stream.good()) {
-        std::cout << c;
-        c = input_stream.get();
+        input_stream.read(reinterpret_cast<char *>(&value), sizeof(value));
+        std::cout << value << std::endl;
     }
 
     input_stream.close();
