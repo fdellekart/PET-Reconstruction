3a5c0ede860ad79e414ececdf649cc4bf0432227
fdellekart <florian.dellekart@hotmail.de>
2024-08-30 10:01:05 +0200

    feat(data read): Add basic code to read file input as stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
+    std::ifstream input_stream;
+
+    input_stream.open("test.txt", std::ifstream::in);
+
+    char c = input_stream.get();
+
+    while (input_stream.good()) {
+        std::cout << c;
+        c = input_stream.get();
+    }
+
+    input_stream.close();
+
+    return 0;
 }
