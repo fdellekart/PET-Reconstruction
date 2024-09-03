6e22a39969df127457ee4b7121e8f4ed4b57f0bc
fdellekart <florian.dellekart@hotmail.de>
2024-09-03 10:58:25 +0200

    feat(main): Use new PETLINKStream class in main
    I think this version will cause a memory leak but I'll try it out to observe

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <fstream>
+#include "PETLINKStream.h"
 
 #define ITIME 1000  // Integration time (interval between two time tags?)
 
 int main(int argc, char **argv) {
-    std::ifstream input_stream;
-
     auto filename = "/home/recon/Documents/Thesis-Dellekart/SIRF-STIR-PET-Reconstruction/input/1.3.12.2.1107.5.2.38.51035.30000017121607191856600000009.bf";
+    PETLINKStream input_stream(filename);
 
-    input_stream.open(filename, std::ifstream::in);
-
-    bool is_prompt; //prompt bit
-    int tag = 0;
     int time_from_start;  // Time since acquisition start in seconds
-    int itagu;
-    int word;
     unsigned long n_prompts = 0;
     unsigned long n_delayeds = 0;
     unsigned long n_singles = 0;
@@ -22,23 +15,24 @@ int main(int argc, char **argv) {
     unsigned long n = 0;
 
     while (input_stream.good()) {
-        input_stream.read(reinterpret_cast<char *>(&word), sizeof(word));
-        if (word > 0) {
-            is_prompt = (word >> 30);
-            if (is_prompt) {
+        EventOrTag* next = input_stream.get_next();
+        
+        if (next->is_event) {
+            Event* event = next->value;
+            if (event->is_prompt) {
                 n_prompts++;
             } else { // delayeds
                 n_delayeds++;
             };
         }
-        else if ((word >> 30) == -3) { // single
-            n_singles++;
+        else {
+            Tag* tag = next->value;
+            if (tag->is_timetag) {
+                n_timetags++;
+                time_from_start = tag->elapsed_millis / ITIME;
+            }
         }
-        else if ((word >> 29) == -4) { // time tag
-            time_from_start = (word & 0x1fffffff) / ITIME;  // assuming that the tag is every 1ms
-            itagu = word - time_from_start * ITIME;
-            n_timetags++;
-        }
+
         n++;
         if ((n_timetags % 100000) == 0) {
             std::cout << "-------------------------------------" << std::endl;
