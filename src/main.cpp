52fc1f8368a74b411ea70d7c69838995ad20ba09
fdellekart <florian.dellekart@hotmail.de>
2024-09-25 18:12:38 +0200

    feat(reading): Decode bin address into tangential, angle and sinogram indexes

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,15 @@
 
 #define ITIME 1000 // Integration time (interval between two time tags?)
 
+#define NSBINS 344
+#define NSANGLES 252
+#define NSINOS 4084
+
 int main(int argc, char **argv) {
   auto filename = "/home/florian/Documents/Programming/MMR2PETSIRD/data/LM/"
                   "17598013_1946_20150604155500.000000.bf";
   PETLINKStream input_stream(filename);
   input_stream.seek_time(600);
-  exit(EXIT_SUCCESS);
 
   if (!input_stream.good()) {
     std::cerr << "File '" << filename << "' not found.";
@@ -30,14 +33,20 @@ int main(int argc, char **argv) {
   DataSummary summary;
   int last_val = 0;
 
+  int32_t bin_address;
+  int32_t tang_pos_num;
+  int32_t rest;
+  int32_t angle_num;
+  int32_t sino_num;
+
   for (std::shared_ptr<EventOrTag> next : input_stream) {
     if (next->is_event) {
-      log << "Event, " << next->event.bin_address << "\n";
-      if (next->event.is_prompt) {
-        summary.n_prompts++;
-      } else { // delayeds
-        summary.n_delayeds++;
-      };
+      bin_address = next->event.bin_address;
+      tang_pos_num = bin_address % NSBINS;
+      rest = bin_address / NSBINS;
+      angle_num = rest % NSANGLES; // view num
+      rest = rest / NSANGLES;
+      sino_num = rest % NSINOS;
     } else {
       if (next->tag.is_timetag) {
         log << "Time, " << next->tag.elapsed_millis << ","
@@ -53,12 +62,9 @@ int main(int argc, char **argv) {
     if (printed_time != time_from_start && ((time_from_start % 10) == 0)) {
       std::cout << "-------------------------------------\n";
       std::cout << "Time: " << time_from_start << "\n";
-      std::cout << "Prompts: " << summary.n_prompts << "\n";
-      std::cout << "Delayeds: " << summary.n_delayeds << "\n";
-      std::cout << "Singles: " << summary.n_singles << "\n";
-      std::cout << "Timetags: " << summary.n_timetags << "\n";
-      std::cout << "Iterations: " << summary.tot_entries << std::endl;
-      printed_time = time_from_start;
+      std::cout << "Tangential: " << tang_pos_num << "\n";
+      std::cout << "Angle: " << angle_num << "\n";
+      std::cout << "Sino: " << sino_num << std::endl;
     }
   }
   log.close();
