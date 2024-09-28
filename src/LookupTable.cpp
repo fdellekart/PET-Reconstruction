2f13b7299397ad3fef6446a02e24a6e50a731adf
fdellekart <florian.dellekart@hotmail.de>
2024-09-28 14:25:43 +0200

    feat(lookuptable): Implement lookup method of lookuptable

--- src/LookupTable.cpp
+++ src/LookupTable.cpp
@@ -7,10 +7,21 @@ int32_t LookupTable::table_idx_to_tang_pos(int32_t table_idx) {
   return table_idx - (det_per_ring / 2) + 1;
 };
 
+int32_t LookupTable::tang_pos_to_table_idx(int32_t tang_pos_num) {
+  return tang_pos_num + (det_per_ring / 2) - 1;
+};
+
+const DetectorPair &LookupTable::lookup(int32_t angle_num,
+                                        int32_t tang_pos_num) {
+  int32_t tang_pos_idx = tang_pos_to_table_idx(tang_pos_num);
+  return (*table)[angle_num][tang_pos_idx];
+};
+
 void LookupTable::initialize_table() {
   for (int32_t ang_pos_num = 0; ang_pos_num < det_per_ring / 2; ang_pos_num++) {
     for (int32_t tang_pos_num = min_tang_pos_num, tang_arr_idx;
          tang_pos_num <= max_tang_pos_num; tang_pos_num++) {
+
       tang_arr_idx = tang_pos_to_table_idx(tang_pos_num);
 
       (*table)[ang_pos_num][tang_arr_idx].det_idx_1 =
