18bd1167f90e2cbc45782ce2e3e9fcbb640a3384
fdellekart <florian.dellekart@hotmail.de>
2024-09-28 14:30:41 +0200

    refactor(lookuptable): Move conversion functions to private
    They are not part of the interface as the table array itself also isn't.
    Therefore, there's no need to have them public.

--- inc/LookupTable.h
+++ inc/LookupTable.h
@@ -12,25 +12,15 @@ struct DetectorPair {
 // and tangential position into a pair of detector indexes
 class LookupTable {
 public:
-  using TableT =
-      std::array<std::array<DetectorPair, NSBINS>, DETECTORS_PER_RING / 2>;
-
   LookupTable() : table(std::make_unique<TableT>()) { initialize_table(); };
 
   // Get the detector pair corresponding to a particular LOR
   const DetectorPair &lookup(int32_t angle_num, int32_t tang_pos_num);
 
-  // Transform detector number to an index in the table
-  //
-  // Tangential positions go from -(N_DET/2)+1 to +N_DET/2
-  // To store it in the table we need a positive integer
-  int32_t tang_pos_to_table_idx(int32_t tang_pos_num);
-
-  // Transform positive integer table index to tangential position number
-  // Goes from -(N_DET/2)+1 to +N_DET/2
-  int32_t table_idx_to_tang_pos(int32_t table_idx);
-
 private:
+  using TableT =
+      std::array<std::array<DetectorPair, NSBINS>, DETECTORS_PER_RING / 2>;
+
   // 2D Array, size: num_angles, num_tangential
   std::unique_ptr<TableT> table;
 
@@ -45,4 +35,14 @@ private:
 
   // Minimum tangential position number
   const int32_t max_tang_pos_num = det_per_ring / 2;
+
+  // Transform detector number to an index in the table
+  //
+  // Tangential positions go from -(N_DET/2)+1 to +N_DET/2
+  // To store it in the table we need a positive integer
+  int32_t tang_pos_to_table_idx(int32_t tang_pos_num);
+
+  // Transform positive integer table index to tangential position number
+  // Goes from -(N_DET/2)+1 to +N_DET/2
+  int32_t table_idx_to_tang_pos(int32_t table_idx);
 };
