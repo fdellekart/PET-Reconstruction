05130eff28dfccb198ef21ca35e17c0c42c8b8b0
fdellekart <florian.dellekart@hotmail.de>
2024-09-28 14:39:03 +0200

    test(lookuptable): Add test to verify detector position lookup works
    For the third pair of EXPECTs the test is failing because det_idx_1
    is zero. This either means a misunderstanding of how the stuff I got
    from STIR should behave, or a of by one error.

--- tests/test_lookuptable.cpp
+++ tests/test_lookuptable.cpp
@@ -1,8 +1,25 @@
+#include "Constants.h"
 #include "LookupTable.h"
 #include <gtest/gtest.h>
 #include <iostream>
 
-TEST(LookupTableTest, TestInitialize) {
+TEST(LookupTableTest, TestLookup) {
   auto LUT = new LookupTable();
+
+  auto det_pair = LUT->lookup(0, 0);
+
+  EXPECT_EQ(det_pair.det_idx_1, 0);
+  EXPECT_EQ(det_pair.det_idx_2, DETECTORS_PER_RING / 2);
+
+  det_pair = LUT->lookup(1, 0);
+
+  EXPECT_EQ(det_pair.det_idx_1, 1);
+  EXPECT_EQ(det_pair.det_idx_2, 253);
+
+  det_pair = LUT->lookup(0, 1);
+
+  EXPECT_EQ(det_pair.det_idx_1, 1);
+  EXPECT_EQ(det_pair.det_idx_2, 251);
+
   delete LUT;
 }
