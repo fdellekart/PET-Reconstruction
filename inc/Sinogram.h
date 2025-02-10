8fb1a1836aea29e9a7d247d894b2c1d9b33bf59c
fdellekart <florian.dellekart@hotmail.de>
2025-02-10 21:19:36 +0100

    feat(sinogram): Add range checks to get_bin and add event function

--- inc/Sinogram.h
+++ inc/Sinogram.h
@@ -1,15 +1,31 @@
+#include <stdexcept>
+
 template <int NANGLES, int NTANG> class Sinogram {
 public:
   Sinogram() = default;
 
   void add_event(int angle_idx, int tang_idx) {
+    check_range(angle_idx, tang_idx);
     (data[angle_idx][tang_idx])++;
   };
 
   int get_bin(int angle_idx, int tang_idx) {
+    check_range(angle_idx, tang_idx);
     return data[angle_idx][tang_idx];
   };
 
 private:
   int data[NANGLES][NTANG] = {0};
+
+  void check_range(int angle_idx, int tang_idx) {
+    if (angle_idx >= NANGLES) {
+      throw std::out_of_range(
+          "Angular idx out of range: " + std::to_string(angle_idx) +
+          " >= " + std::to_string(NANGLES));
+    } else if (tang_idx >= NTANG) {
+      throw std::out_of_range(
+          "Tangential idx out of range: " + std::to_string(tang_idx) +
+          " >= " + std::to_string(NTANG));
+    }
+  };
 };
--- tests/test_sinogram.cpp
+++ tests/test_sinogram.cpp
@@ -1,6 +1,7 @@
 #include "Sinogram.h"
 #include <gtest/gtest.h>
 #include <iostream>
+#include <stdexcept>
 
 TEST(SinogramTest, TestSinogram) {
   // Setup
@@ -10,6 +11,12 @@ TEST(SinogramTest, TestSinogram) {
   sinogram.add_event(1, 1);
   sinogram.add_event(1, 1);
 
+  sinogram.add_event(9, 9);
+  sinogram.add_event(9, 9);
+
   // Verify
   EXPECT_EQ(sinogram.get_bin(1, 1), 2);
+  EXPECT_EQ(sinogram.get_bin(9, 9), 2);
+  EXPECT_THROW(sinogram.add_event(10, 10), std::out_of_range);
+  EXPECT_THROW(sinogram.get_bin(10, 10), std::out_of_range);
 }
