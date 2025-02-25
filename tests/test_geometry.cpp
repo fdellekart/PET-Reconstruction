b4dfd11a200fd3db2664ea492f194649ece798d5
fdellekart <florian.dellekart@hotmail.de>
2025-02-25 11:23:24 +0100

    test(lor): Add test to verify working detector positions for vertical LOR

--- tests/test_geometry.cpp
+++ tests/test_geometry.cpp
@@ -10,3 +10,14 @@ TEST(LORTest, TestLORDetectorPosition) {
   EXPECT_NEAR(result.second.x, 0.02506281, 0.00001);
   EXPECT_DOUBLE_EQ(result.second.y, 0.5);
 }
+
+// Veritcal LOR through scanner center
+TEST(LORTest, TestLORDetectorPositionVertical) {
+  auto lor = LOR::from_idxs(172, 126, 0);
+  ScannerGeometry geometry = ScannerGeometry::mmr();
+  auto result = lor.get_det_positions(geometry);
+  EXPECT_DOUBLE_EQ(result.first.x, geometry.detector_diameter / 2);
+  EXPECT_DOUBLE_EQ(result.first.y, geometry.detector_diameter / 2);
+  EXPECT_DOUBLE_EQ(result.second.x, geometry.detector_diameter / 2);
+  EXPECT_DOUBLE_EQ(result.second.y, -geometry.detector_diameter / 2);
+}
