f0837ddf618740a2254f2f750c7da60ff875ae42
fdellekart <florian.dellekart@hotmail.de>
2025-03-14 14:22:59 +0100

    tryout: Basic expectation maximization routine

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <chrono>
 #include <cstdint>
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -18,11 +19,34 @@ int main(int argc, char **argv) {
   ScannerGeometry geometry = ScannerGeometry::mmr();
   RayTracer tracer(geometry.img_dimensions);
   LOR lor;
-  Image<344, 344> image;
+  Image<344, 344> image(1.0);
+  Sinogram<252, 344> forwardprojection;
+  Sinogram<252, 344> errors;
+  Image<344, 344> backward_errors;
+  char filename[1000];
 
   auto t1 = std::chrono::high_resolution_clock::now();
-  image = project_backward(sinogram, tracer, geometry);
-  sinogram = project_forward<252, 344>(image, tracer, geometry);
+  for (int it = 0; it < 20; it++) {
+    std::cout << "Iteration " << it << std::endl;
+
+    forwardprojection = project_forward<252, 344>(image, tracer, geometry);
+
+    for (int i = 0; i < 252; i++) {
+      for (int j = 0; j < 344; j++) {
+        errors.data[i][j] = sinogram.data[i][j] / forwardprojection.data[i][j];
+      }
+    }
+
+    backward_errors = project_backward(errors, tracer, geometry);
+    for (int i = 0; i < 344; i++) {
+      for (int j = 0; j < 344; j++) {
+        image.data[i][j] *= backward_errors.data[i][j];
+      }
+    }
+    snprintf(filename, sizeof(filename),
+             "/home/florian/Documents/Programming/MMR2PETSIRD/image_it%d", it);
+    image.to_file(filename);
+  }
 
   auto t2 = std::chrono::high_resolution_clock::now();
   auto delta_t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
@@ -33,10 +57,6 @@ int main(int argc, char **argv) {
       std::ios_base::app);
 
   dur_file << delta_t << std::endl;
-
-  image.to_file("/home/florian/Documents/Programming/MMR2PETSIRD/image");
-  sinogram.to_file("/home/florian/Documents/Programming/MMR2PETSIRD/"
-                   "sinogram_forward_project");
 
   return 0;
 }
